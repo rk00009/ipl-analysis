9ac5c87e9eb0f0610dcad055ff1d44a142e99fe8
rk00009 <rajatkar9582679590@gmail.com>
2026-05-06 23:18:20 +0530

    chat 1 is completed

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv(r'c:\91931\archive\IPL_Matches_2022.csv')
 
@@ -19,14 +21,26 @@ print(df.isnull().sum())
 # print(df.isnull().sum())
 
 
-most_wins = df['WinningTeam'].value_counts()
-print("Most winning team:\n", most_wins.head(10))
+# most_wins = df['WinningTeam'].value_counts()
+# print("Most winning team:\n", most_wins.head(10))
 
-host_city = df['City'].value_counts()
-print("Most Hosted City:\n", host_city.head())
+# host_city = df['City'].value_counts()
+# print("Most Hosted City:\n", host_city.head())
 
-df["toss_match_win"]= df["TossWinner"] == df["WinningTeam"]
+# df["toss_match_win"]= df["TossWinner"] == df["WinningTeam"]
 
-print(df["toss_match_win"].value_counts())
+# print(df["toss_match_win"].value_counts())
 
-print(df["Player_of_Match"].value_counts().head(10))
+# print(df["Player_of_Match"].value_counts().head(10))
+
+plt.figure(figsize=(10,5))
+top_teams = df['WinningTeam'].value_counts().head(8)
+sns.barplot(x=top_teams.index, y=top_teams.values)
+plt.title('IPL - Most Wins by Team', fontsize=14, fontweight='bold')
+plt.xlabel('Team')
+plt.ylabel('Number of Wins')
+plt.tight_layout()
+plt.xticks(rotation=45, ha='right')
+plt.savefig('team_wins.png')
+plt.show()
+print("Chart 1 saved!")
